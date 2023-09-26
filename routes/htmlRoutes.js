4c285087bdd1163868deb4dea65af72a8e0ef2fe
Cossack <kkowalak9@gmail.com>
2023-09-25 21:59:44 -0700

    Finished htmlRoutes.js

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,11 @@
+//Requiring all need packages and locations
+const path = require("path");
+const app = require("express").Router();
+
+//Path for notes.html
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+  
+module.exports = app;
+  
